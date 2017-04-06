46e50724a12de24502fdb3256136e5d6aafe328b
IamHungDo <hung95do@yahoo.com>
2017-04-05 19:50:09 -0700

    Animations added

--- TinderApp/CardsViewController.swift
+++ TinderApp/CardsViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class CardsViewController: UIViewController {
 
+    @IBOutlet weak var pictureView: UIImageView!
+    var trayOriginalCenter: CGPoint!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
     }
@@ -21,6 +26,52 @@ class CardsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
+        
+        
+        
+        let location = sender.location(in: view)
+        let velocity = sender.velocity(in: view)
+        let translation = sender.translation(in: view)
+        
+        if sender.state == .began {
+            print("Gesture began")
+            trayOriginalCenter = pictureView.center
+
+        } else if sender.state == .changed {
+            print("Gesture is changing")
+            
+            if (translation.x < 0) {
+                
+                UIView.animate(withDuration: 0.3, animations: {
+                    let xRadians = CGFloat( -50 * M_PI / 180)
+                    self.pictureView.transform = CGAffineTransform(rotationAngle: xRadians)
+                }) { (Bool) in
+                    if(translation.x > 50) {
+                        self.pictureView.center.x += 320
+                        
+                    }                }
+                
+ 
+                
+            } else {
+                UIView.animate(withDuration: 0.3, animations: {
+                    let xRadians = CGFloat( -50 * M_PI / 180)
+                    self.pictureView.transform = CGAffineTransform(rotationAngle: xRadians)
+                }) { (Bool) in
+                    if(translation.x > -50) {
+                        self.pictureView.center.x += -320
+                        
+                    }                }
+            }
+            
+
+
+        } else if sender.state == .ended {
+            print("Gesture ended")
+        }
+    }
 
     /*
     // MARK: - Navigation
